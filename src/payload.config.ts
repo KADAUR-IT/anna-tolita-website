8bbbb1beaac6753460db4ec8e10454f8b51cc7eb
Kyllian Marie-Magdelaine <kmariemagdelaine@kadaur.com>
2025-10-29 15:41:32 +0100

    Integrate Vercel Blob

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,6 +2,7 @@
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob"
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
@@ -32,6 +33,13 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN || ''
+    }),
     // storage-adapter-placeholder
   ],
 })
