c028b62e9453866bf012a9d18c27ddb5a567a960
Kyllian Marie-Magdelaine <kmariemagdelaine@kadaur.com>
2025-12-05 12:06:04 +0100

    Updated Next config to use Webpack instead of Turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,33 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: [
+    'pino', 
+    'pino-pretty', 
+    'lokijs', 
+    'pino-elasticsearch', 
+    'payload',           // <-- Indispensable
+    '@payloadcms/db-mongodb', // <-- Mets ici ton adaptateur (mongodb ou postgres)
+    'sharp'              // <-- Souvent la cause de problèmes d'images
+  ],
+
   // Your Next.js config here
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
+    }
+
+    webpackConfig.resolve.alias = {
+      ...webpackConfig.resolve.alias,
+      "pino-pretty": false,
+      "lokijs": false,
+      "desm": false,
+      "fastbench": false,
+      "pino-elasticsearch": false,
+      "tap": false,
+      "pino-std-serializers": false
     }
 
     return webpackConfig
