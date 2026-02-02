fa4f9d6111fd2029db7ceeede8e54a3eb2745cd0
Kyllian Marie-Magdelaine <kmariemagdelaine@kadaur.com>
2026-02-02 11:09:29 +0100

    fix: users collection

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,10 +2,6 @@ import type { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
   slug: 'users',
-  access:
-  {
-      read: () => {return false}
-  },
   admin: {
     useAsTitle: 'email',
   },
